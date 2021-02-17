e04be763178d4f4b473dcea839005d5058cfbc98
sayuru17 <77736977+sayuru17@users.noreply.github.com>
2021-02-17 12:16:41 +0530

    Create DummyServer.java

---
+++ Client Server Architecture/Tutorial 1/DummyServer.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tutorial1;
+
+/**
+ *
+ * @author ASUS ZenBook
+ */
+public class DummyServer {
+    
+    String testConnection(){
+        System.out.println("DummyServer. Connection being tested...");
+        return "Connection Succeded";
+    }
+}
